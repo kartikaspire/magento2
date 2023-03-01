3cd7ab3a5066afb3c2a21211c54dc5304c995855
Kartik Samantaray <kartik.samantaray@aspiresys.com>
2023-03-01 20:43:55 +0530

    customer synchronize

--- app/code/Aspire/Module/Controller/Adminhtml/Sync/Index.php
+++ app/code/Aspire/Module/Controller/Adminhtml/Sync/Index.php
@@ -2,31 +2,58 @@
 
 namespace Aspire\Module\Controller\Adminhtml\Sync;
 
+use Magento\Customer\Model\ResourceModel\Customer\CollectionFactory;
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Registry;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Ui\Component\MassAction\Filter;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Aspire\Module\Block\Adminhtml\ApiCall;
+use Magento\Customer\Api\CustomerRepositoryInterface;
 
-class Index extends \Magento\Backend\App\Action
+class Index extends Action
 {
-	const XML_CONFIGURATION_SETTING = 'module/configuration/enable';
-    const XML_CONFIGURATION_APIURL = 'module/configuration/api_url';
-    const XML_API_USERNAME = 'module/configuration/api_username';
-    const XML_API_PASSWORD = 'module/configuration/api_password';
-
+    protected $_coreRegistry = null;
+    protected $resultPageFactory;
+    protected $CollectionFactory;
+    protected $apiCall;
+    protected $customerRepositoryInterface;
     /**
-  	* @var \Magento\Framework\App\Config\ScopeConfigInterface
-  	*/
-  	protected $scopeConfig;
-  	public function __construct(
-    ScopeConfigInterface $scopeConfig
-  	) {
-		$this->scopeConfig = $scopeConfig;
-	}
-	public function execute() {
-		echo "Hi";
-		echo "By";
-		$storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
-    $apiUrl = $this->scopeConfig->getValue(self::XML_CONFIGURATION_APIURL, $storeScope);
-    $apiUsername = $this->scopeConfig->getValue(self::XML_API_USERNAME, $storeScope);
-		//echo $this->apiResponse->getApiResponse();
-	}
+  * @var \Magento\Framework\App\Config\ScopeConfigInterface
+  */
+  protected $scopeConfig;
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory,
+        Registry $registry,
+        Filter $filter,
+        CollectionFactory $CollectionFactory,
+        ScopeConfigInterface $scopeConfig,
+        ApiCall $apiCall,
+        CustomerRepositoryInterface $customerRepositoryInterface
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        $this->_coreRegistry = $registry;
+        $this->CollectionFactory = $CollectionFactory;
+        $this->filter = $filter;
+        $this->scopeConfig = $scopeConfig;
+        $this->apiCall = $apiCall;
+        $this->customerRepositoryInterface = $customerRepositoryInterface;
+        parent::__construct($context);
+    }
+    public function execute()
+    {
+        $collection = $this->filter->getCollection($this->CollectionFactory->create());
+
+        $count = 0;
+        foreach ($collection as $child) {
+            $result = $this->apiCall->getApiCustomerStatus($child->getData('entity_id'));
+            $customer = $this->customerRepositoryInterface->getById($child->getData('entity_id'));
+            $customer->setData('customer_apistatus', $result);
+            $customer->save();
+            $count++;
+        }
+        exit;
+    }
 }
-?>
